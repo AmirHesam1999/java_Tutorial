54dabb945984be0d14f5f9ef68826994a95d78bf
Hesam <amirhesamhashemi4@gmail.com>
2024-11-21 13:36:33 -0500

    Text must be wrapped inside double quotations marks "".If you forget the double quotes, an error occurs

---
+++ Tutorial/Output_or_Print/Print_Text/Double_Quotes/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This sentence will work!");
+        System.out.println(This sentence will produce an error);
+    }
+}
