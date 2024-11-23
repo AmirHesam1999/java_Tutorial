ec678500335b8f6099d4574beec1d24580f330ce
Hesam <amirhesamhashemi4@gmail.com>
2024-11-23 13:37:14 -0500

    Multi-line comments start with /* and ends with */. Any text between /* and */ will be ignored by Java.

---
+++ Tutorial/comments/Multi-line/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /* The code below will print the words Hello World
+        to the screen, and it is amazing */
+        System.out.println("Hello World");
+    }
+}
