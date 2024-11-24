ff2c8aeba714b275eee012dc267ee26fe7757287
Hesam <amirhesamhashemi4@gmail.com>
2024-11-24 12:01:44 -0500

    If you don't want others (or yourself) to overwrite existing values, use the final keyword (this will declare the variable as "final" or "constant", which means unchangeable and read-only)

---
+++ Tutorial/Java_Variables/Variables/Final_Variables/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        final int myNum = 25;
+        myNum = 20; // will generate an error
+        System.out.println(myNum);
+    }
+}
