e4195eab749c3f86dce1b8e7b3448873d0c34919
Hesam <amirhesamhashemi4@gmail.com>
2024-11-26 09:09:42 -0500

    You can also use the + character to add a variable to another variable

---
+++ Tutorial/Java_Variables/Print_Variables2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args){
+		String firstname = "AmirHesam";
+		String lastName = "Hashemi";
+		String fullName = firstname + lastName;
+		System.out.println(fullName);
+	}
+}
