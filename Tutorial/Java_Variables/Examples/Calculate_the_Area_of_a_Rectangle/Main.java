36a8d1256f95c29d19df331527762fcf30290fc7
Hesam <amirhesamhashemi4@gmail.com>
2024-11-26 11:47:28 -0500

    In this real-life example, we create a program to calculate the area of a rectangle (by multiplying the length and width)

---
+++ Tutorial/Java_Variables/Examples/Calculate_the_Area_of_a_Rectangle/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String[] args){
+		// Create integer variables
+		int lenght = 4;
+		int width = 6;
+		int area;
+
+		//Calculate the area of a rectangle
+		area = lenght * width;
+
+		// Print variables
+		System.out.println("Lenght is: " + lenght);
+		System.out.println("Width is: " + width);
+		System.out.println("Area of the rectangle is: " + area);
+	}
+}
