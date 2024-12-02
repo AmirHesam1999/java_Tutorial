d917a706488b663d3c82a0a960ba6e256dd32787
Hesam <amirhesamhashemi4@gmail.com>
2024-12-02 11:44:07 -0500

    You should use a floating point type whenever you need a number with a decimal, such as 9.99 or 3.14515.The float and double data types can store fractional numbers. Note that you should end the value with an f for floats and d for doubles

---
+++ Tutorial/Java_Data_Types/Numbers/Floating_Point_Types/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float myNum=5.75f;
+        System.out.println(myNum);
+        System.out.println("------------------------------");
+        double myNum2 = 19.99d;
+        System.out.println(myNum2);
+    }
+}
