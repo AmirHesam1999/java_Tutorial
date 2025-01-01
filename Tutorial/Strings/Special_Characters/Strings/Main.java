6abb110087140b34f20b9fccfda0af7042edc3ad
Hesam <amirhesamhashemi4@gmail.com>
2025-01-01 08:05:58 -0500

    Because strings must be written within quotes, Java will misunderstand this string, and generate an error The solution to avoid this problem, is to use the backslash escape character.The backslash () escape character turns special characters into string characters

---
+++ Tutorial/Strings/Special_Characters/Strings/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String txt = "We are the so-called \"Viking\" from the north.";
+        System.out.println(txt);
+    }
+}
