4992f2879c1b4b99dfffdda17402edf486a3045d
Hesam <amirhesamhashemi4@gmail.com>
2025-01-01 07:55:29 -0500

    If you add a number and a string, the result will be a string concatenation

---
+++ Tutorial/Strings/Numbers_and_Strings/Adding3/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String x = "10";
+        int y = 20;
+        String z = x + y;
+        System.out.println(z);
+    }
+}
